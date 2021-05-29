5d6aef2f53111579db777f8e4f31a4331529718a
Winstler <84248453+Winstler@users.noreply.github.com>
2021-05-29 22:02:12 +0300

    Add files via upload

--- taxes.js
+++ taxes.js
@@ -4,23 +4,23 @@ if (income<=9950)
 	{
 		taxRate = 0.1;
 	}
-else if (9950<income || income<=40525)
+else if (9950<income && income<=40525)
 	{
 		taxRate = 0.12;
 	}
-else if (40525<income || income<=86375)
+else if (40525<income && income<=86375)
 	{
 		taxRate = 0.22;
 	}
-else if (86375<income || income<=164925)
+else if (86375<income && income<=164925)
 	{
 		taxRate = 0.24;
 	}
-else if (164925<income || income<=209425)
+else if (164925<income && income<=209425)
 	{
 		taxRate = 0.32;
 	}
-else if (209425<income || income<=523600)
+else if (209425<income && income<=523600)
 	{
 		taxRate = 0.35;
 	}
